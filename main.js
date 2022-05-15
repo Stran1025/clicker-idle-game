4b1b52bc9fe5b6ce87dc02d578d1ae73ab5091de
Sang Tran <sang.tran1025@gmail.com>
2022-05-15 21:01:15 +0000

    miner card in place and most of buying miner functionaltiy

--- main.js
+++ main.js
@@ -6,7 +6,14 @@ const data = {
     price: 10,
     increaseRate: 1.05,
     acceleration: 0.05
+  },
+  miner: {
+    count: 0,
+    price: 20,
+    increaseRate: 1.1,
+    acceleration: 0.075
   }
+
 };
 
 const $block = document.querySelector('#block');
@@ -18,6 +25,13 @@ const $pickaxePrice = document.querySelector('#pickaxe-price');
 const $pickaxeCount = document.querySelector('#pickaxe-count');
 $pickaxeBuy.addEventListener('click', event => {
   buyPickaxe();
+});
+
+const $minerBuy = document.querySelector('#miner-buy');
+const $minerPrice = document.querySelector('#miner-price');
+const $minerCount = document.querySelector('#miner-count');
+$minerBuy.addEventListener('click', event => {
+  buyMiner();
 });
 
 function increment(data, rate) {
@@ -37,5 +51,19 @@ function buyPickaxe() {
   data.clickRate++;
   data.pickaxe.increaseRate += data.pickaxe.acceleration;
   $pickaxePrice.textContent = data.pickaxe.price;
+  $display.textContent = data.blocks;
+}
+
+function buyMiner() {
+  if (data.blocks < data) {
+    return;
+  }
+  data.blocks -= data.miner.price;
+  data.miner.price *= data.miner.increaseRate;
+  data.miner.price = Math.trunc(data.miner.price);
+  data.miner.count++;
+  $minerCount.textContent = data.miner.count;
+  data.miner.increaseRate += data.miner.acceleration;
+  $minerPrice.textContent = data.miner.price;
   $display.textContent = data.blocks;
 }
