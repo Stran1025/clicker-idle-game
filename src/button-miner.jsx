2fdf848459de2ec20bf4b96518ea2f021cc912be
Sang Tran <sang.tran1025@gmail.com>
2022-05-19 23:49:09 +0000

    completed button module

--- src/button-miner.jsx
+++ src/button-miner.jsx
@@ -4,15 +4,24 @@ class MinerButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = { count: 0 };
+    this.handleMining = this.handleMining.bind(this);
   }
 
   handleMining() {
     let c = this.state.count;
+    c++;
+    this.setState({ count: c });
   }
 
   render() {
     return (
-          <button className='btn btn-primary w-100' onClick={this.handleMining}>Mine</button>      </div>
+      <div className='row justify-content-center mt-5'>
+        <div className='col-1 align-self-center'>
+          <button className='btn btn-primary w-100' onClick={this.handleMining}>Mine</button>
+        </div>
+      </div>
     );
   }
 }
+
+export default MinerButton;
---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MinerButton from './button-miner';
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+root.render(<MinerButton/>);
